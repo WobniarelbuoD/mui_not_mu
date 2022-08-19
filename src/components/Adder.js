d99b9dafaa703e36ffc2ecae4a4fff7158177799
WobniarelbuoD <106962703+WobniarelbuoD@users.noreply.github.com>
2022-08-19 13:09:42 +0300

    Adder Update
    Fixed adder functionality to add and remove items from array using useState.

--- src/components/Adder.js
+++ src/components/Adder.js
@@ -1,29 +1,41 @@
-import { Button, Divider, List, ListItem, ListItemButton, ListItemText, TextField, Typography } from "@mui/material";
+import {Button,Divider,List,ListItem,ListItemButton,ListItemText,TextField,Typography,} from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
 
-
 const Adder = () => {
-    const [input, inputUpdate] = useState()
-    const [items, setItems] = useState([])
+  const [input, inputUpdate] = useState();
+  const [items, setItems] = useState([]);
 
-    const arrayUpdate = () =>{
-        setItems([...items, input])
-    }
+  const arrayUpdate = () => {
+    setItems([...items, input]);
+    console.log(items)
+  };
 
-    return(
-  <Container >
-    <Typography variant="h2">Task Master</Typography>
-    <Divider sx={{whiteSpace:"pre-wrap", mt:10}}>
-    <TextField onChange={e => inputUpdate(e.target.value)} label="Text"></TextField>
-    <Button onClick={() => arrayUpdate()} variant="contained" sx={{mt:5}}>Add</Button>
-    <List sx={{mt:5}}>
-        {/* {Items.map(item =>{
-            return(<ListItem key={item}><ListItemText primary={item}></ListItemText></ListItem>)})} */}
-    </List>
-    </Divider>
-  </Container>
-)
+  const remove = (index) =>{
+    items.splice(index,1)
+    setItems([...items])
+    console.log(items)
+  }
+
+  return (
+    <Container>
+      <Typography variant="h2">Task Master</Typography>
+      <Divider sx={{ whiteSpace: "pre-wrap", mt: 10 }}>
+        <TextField
+          onChange={(e) => inputUpdate(e.target.value)}
+          label="Text"
+        ></TextField>
+        <Button onClick={() => arrayUpdate()} variant="contained" sx={{ mt: 5 }}>Add</Button>
+      <List sx={{ mt: 5 }}>
+        {items.map((item,index) => {
+          return (
+            <ListItem key={index}><ListItemText primary={item}></ListItemText><ListItemButton onClick={() => remove(index)}>Delete</ListItemButton></ListItem>
+          );
+        })}
+      </List>
+      </Divider>
+    </Container>
+  );
 };
 
 export default Adder;
